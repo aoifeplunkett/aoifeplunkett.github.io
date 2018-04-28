b2ff9d5f6fd931d56ed83798218d28b34863fe31
Aoife Plunkett <38736427+aoifeplunkett@users.noreply.github.com>
2018-04-27 19:33:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 $(function() {
 
-    $(window).scroll(function() {
-		
-		var distanceScrolled = Math.max(0, $(window).scrollTop());
-		var $navbar = $('nav');
-		var $navbarWrapper = $('.navbar-wrapper')
+   window.onscroll = function() {scrollFunction()};
 
-		var pinPoint = $navbarWrapper.offset().top;
-
-		if (distanceScrolled >= pinPoint) {
-			$navbar.addClass('pinned');
-		} else {
-			$navbar.removeClass('pinned')
-		}
-	});
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
     
 	$('a[href*="#"]').click(function(e) {
 		e.preventDefault();
